aee4aa25242c90f6181cb340a6dd835f2fe9aaad
Kristoffer <kristofferlars1@gmail.com>
2015-09-24 14:42:06 +0200

    Working services

--- src/steadycam.cpp
+++ src/steadycam.cpp
@@ -3,28 +3,95 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Quaternion.h"
 #include "steadycam/Control.h"
-#include <boost/algorithm/string.hpp>
+#include "steadycam/setRunning.h"
+#include "steadycam/getRunning.h"
+#include "steadycam/setControlMode.h"
+#include "steadycam/getControlMode.h"
+#include "steadycam/setEulerAngles.h"
+#include "steadycam/getEulerAngles.h"
+#include "steadycam/setPoint.h"
+#include "steadycam/getPoint.h"
 
 using namespace std;
 using namespace ros;
+
 void imuCallback(const geometry_msgs::Quaternion::ConstPtr& msg)
 {
     //on do callback from IMU data
 }
 bool running = false;
+string control_mode = "";
 
-void comfigCallBack(const steadycam::Control::ConstPtr&  msg)
+bool setRunningCallBack(steadycam::setRunning::Request &req,
+                        steadycam::setRunning::Response &res)
 {
-    bool running = msg->on_off.data;
-    ROS_INFO("%i",running);
+    ROS_INFO("I got %i",req.running);
+    running = req.running;
+    return true;
 }
+
+bool setPointCallBack(steadycam::setPoint::Request &req,
+                      steadycam::setPoint::Response &res)
+{
+    return true;
+}
+bool setEulerAnglesCallBack(steadycam::setEulerAngles::Request &req,
+                            steadycam::setEulerAngles::Response &res)
+{
+    return true;
+}
+bool setControlModeCallBack(steadycam::setControlMode::Request &req,
+                            steadycam::setControlMode::Response &res)
+{
+    control_mode = req.controlMode;
+    return true;
+}
+
+bool getControlModeCallBack(steadycam::getControlMode::Request &req,
+                            steadycam::getControlMode::Response &res)
+{
+    res.controlMode = control_mode;
+    return true;
+}
+
+bool getEulerAnglesCallBack(steadycam::getEulerAngles::Request &req,
+                            steadycam::getEulerAngles::Response &res)
+{
+    return true;
+}
+
+bool getPointCallBack(steadycam::getPoint::Request &req,
+                      steadycam::getPoint::Response &res)
+{
+    return true;
+}
+
+bool getRunningCallBack(steadycam::getRunning::Request &req,
+                        steadycam::getRunning::Response &res)
+{
+    res.running = running;
+    return true;
+}
+
+
 int main(int argc, char **argv)
 {
     init(argc,argv,"steadycam");
     NodeHandle n;
+
     Publisher pub = n.advertise<geometry_msgs::Twist>("steadycam_servo_control",1000);
     Subscriber sub = n.subscribe("steadycam_imu_data",1000,imuCallback);
-    Subscriber comfig_sub = n.subscribe("steadycam_control",1000,comfigCallBack);
+
+
+    ServiceServer service1 = n.advertiseService("setRunning", setRunningCallBack);
+    ServiceServer service2_ = n.advertiseService("setPoint",setPointCallBack);
+    ServiceServer service3 = n.advertiseService("setEulerAngles",setEulerAnglesCallBack);
+    ServiceServer service4 = n.advertiseService("setControlMode",setControlModeCallBack);
+    ServiceServer service5 = n.advertiseService("getRunning",getRunningCallBack);
+    ServiceServer service6 = n.advertiseService("getPoint",getPointCallBack);
+    ServiceServer service7 = n.advertiseService("getEulerAngles",getEulerAnglesCallBack);
+    ServiceServer service8 = n.advertiseService("getControlMode",getControlModeCallBack);
+
     Rate loop_rate(60);
 
     float count = 0.0;
@@ -47,6 +114,9 @@ int main(int argc, char **argv)
     }
     return 0;
 }
+
+
+
 
 
 
--- src/steadycam_test.cpp
+++ src/steadycam_test.cpp
@@ -1,4 +1,5 @@
-
+#include <steadycam/getRunning.h>
+#include <steadycam/setRunning.h>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
@@ -12,32 +13,48 @@ using namespace geometry_msgs;
 
 // mode-false = angle lock, true = position lock
 bool sender = false;
+int seq = 0;
+
+
+//builds the message that is to be sendt to the arduino servo controller.
+steadycam::Control buildPackage(bool on_off,string control_type,double angle_lock[], double position_lock[])
+{
+    steadycam::Control msg;
+    msg.on_off.data = on_off;
+    msg.control_type = control_type;
+    msg.angle_lock.header.stamp = ros::Time::now();
+    msg.angle_lock.header.seq = seq;
+    msg.angle_lock.quaternion.x = angle_lock[0];
+    msg.angle_lock.quaternion.y = angle_lock[1];
+    msg.angle_lock.quaternion.z = angle_lock[2];
+    msg.position_lock.header.stamp = ros::Time::now();
+    msg.position_lock.header.seq = seq;
+    msg.position_lock.point.x = position_lock[0];
+    msg.position_lock.point.y = position_lock[1];
+    msg.position_lock.point.z = position_lock[2];
+    seq += 1;
+    return msg;
+}
+
 
 int main(int argc, char **argv)
 {
     init(argc,argv,"steadycam_test");
     NodeHandle n;
     Rate loop_rate(1);
-    Publisher pub = n.advertise<steadycam::Control>("steadycam_control",1000);
-    steadycam::Control msg;
-    std_msgs::Bool test;
-
+    ServiceClient setRunning_client = n.serviceClient<steadycam::setRunning>("setRunning");
+    steadycam::setRunning srv;
     while(ros::ok())
     {
-        test.data = sender;
+        srv.request.running = sender;
         sender = !sender;
-        msg.on_off = test;
-        pub.publish(msg);
+        setRunning_client.call(srv);
         spinOnce();
         loop_rate.sleep();
     }
     return 0;
 }
 
-steadycam::Control buildPackage(bool on_off,string control_type,Quaternion angle_lock, Point position_lock)
-{
-
-}
 
 
 
